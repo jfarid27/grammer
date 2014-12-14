fe003c508a5bc4d6e93861e4b043be7829c8076f
jfarid27 <jrab227@gmail.com>
2014-12-14 14:25:09 -0500

    testing adding

---
+++ test/test.config.js
@@ -0,0 +1,31 @@
+(function(){
+var allTestFiles = [];
+var TEST_REGEXP = /spec\.js$/;
+
+var pathToModule = function(path) {
+  return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+}
+
+Object.keys(window.__karma__.files).forEach(function(file) {
+  if (TEST_REGEXP.test(file)) {
+    // Normalize paths to RequireJS module names.
+    allTestFiles.push(pathToModule(file));
+  }
+})
+
+require.config({
+
+    'baseUrl': '/base',
+
+    'paths': {},
+
+    'shim': {},
+
+    'waitSeconds': 6,
+
+    'deps': allTestFiles,
+
+    'callback': window.__karma__.start
+})
+
+})();
